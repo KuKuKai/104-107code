5c82078a189566e4c48663482e9c52951aea3774
KuKuKai <104021026@live.asia.edu.tw>
2015-08-03 14:10:48 +0800

    new
    new

--- FaceJava/src/FaceJave4.java
+++ FaceJava/src/FaceJave4.java
@@ -4,16 +4,16 @@ public class FaceJave4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            System.out.println("¡@¡@¢~¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢¡¡@¡@");
-           System.out.println("¡@¡@¢x                 ¢x   ");
+           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@ ¢x   ");
            System.out.println("¡@¡@¢¥¢¤¢¤¢¤¢¤¢¤¢¤¡ó¢¤¡ó¢¤¢¤¢¤¢¤¢¤¢¤¢§   ");
-           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@          ¢x   ");
-           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@          ¢x   ");
-           System.out.println("¢~¢¤¢t¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¢u¢w¢¡");
+           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@ ¢x   ");
+           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡³¡³¡@¡@¡@            ¢x   ");
+           System.out.println("¢~¢¤¢t¡@¡@¡@¡@   ¢¢¢w¢w¢£¡@¡@¡@¡@  ¢u¢w¢¡");
            System.out.println("¢x ¢~¢x         ¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¢x¢¡¢x");
            System.out.println("¢x ¢x¢p¢p¢p¢p¢p¢p¢p¢p¢p¢x¢x ¢x");
            System.out.println("¢x ¢x¢p¢p¢p¢p¢p¢p¢p¢p¢p¢x¢x ¢x");
-           System.out.println("¢¢¢w¢£¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢¢¢w¢£");
-           System.out.println("   ¡@   ¡@¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p               ");
+           System.out.println("¢¢¢w¢£¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p   ¢¢¢w¢£");
+           System.out.println("   ¡@   ¡@¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p                  ");
            System.out.println(" ¡@¡@¡@¡@¡@¡@¢y ¢y            ¢y ¢y      ¡@¡@¡@¡@¡@   ");
            System.out.println(" ¡@¡@¡@¡@¡@¡@¢y ¢y            ¢y ¢y      ¡@¡@¡@¡@¡@     ");
            System.out.println("      ¡@¡@¡@¡@¢~ùö   ùö¢¡ ¢~ ùö  ùö ¢¡                         ");
