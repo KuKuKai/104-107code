d49b6e76b257b80b2d185f6c1757a37140d5e872
KuKuKai <104021026@live.asia.edu.tw>
2015-08-03 14:03:23 +0800

    Notreally
    I'm serious

---
+++ FaceJava/src/FaceJava.java
@@ -0,0 +1,14 @@
+
+public class FaceJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("=========================");
+           System.out.println("* 姓名: 劉錦政                                            * ");
+           System.out.println("* 系所: 亞洲大學資工系資電阻            * ");
+           System.out.println("* 職級: 菜逼八新生                                  * ");
+           System.out.println("* 電話: 0978206086      * ");
+           System.out.println("=========================");
+	}
+
+}
---
+++ FaceJava/src/FaceJave4.java
@@ -0,0 +1,23 @@
+
+public class FaceJave4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("¡@¡@¢~¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢¡¡@¡@");
+           System.out.println("¡@¡@¢x                 ¢x   ");
+           System.out.println("¡@¡@¢¥¢¤¢¤¢¤¢¤¢¤¢¤¡ó¢¤¡ó¢¤¢¤¢¤¢¤¢¤¢¤¢§   ");
+           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@          ¢x   ");
+           System.out.println("¡@¡@¢x¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@          ¢x   ");
+           System.out.println("¢~¢¤¢t¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¢u¢w¢¡");
+           System.out.println("¢x ¢~¢x         ¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¢x¢¡¢x");
+           System.out.println("¢x ¢x¢p¢p¢p¢p¢p¢p¢p¢p¢p¢x¢x ¢x");
+           System.out.println("¢x ¢x¢p¢p¢p¢p¢p¢p¢p¢p¢p¢x¢x ¢x");
+           System.out.println("¢¢¢w¢£¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢¢¢w¢£");
+           System.out.println("   ¡@   ¡@¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p               ");
+           System.out.println(" ¡@¡@¡@¡@¡@¡@¢y ¢y            ¢y ¢y      ¡@¡@¡@¡@¡@   ");
+           System.out.println(" ¡@¡@¡@¡@¡@¡@¢y ¢y            ¢y ¢y      ¡@¡@¡@¡@¡@     ");
+           System.out.println("      ¡@¡@¡@¡@¢~ùö   ùö¢¡ ¢~ ùö  ùö ¢¡                         ");
+           System.out.println("      ¡@¡@¡@¡@¢¢¢w¢w¢w¢£¢¢¢w¢w¢w¢£                        ");
+	}
+
+}
