a4e4c380c88bb28a0da01ac0b65985454c497619
KuKuKai <104021026@live.asia.edu.tw>
2015-08-11 15:58:05 +0800

    凸

---
+++ 20150811/src/凸.java
@@ -0,0 +1,22 @@
+
+public class ¥Y {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡@¡@¡@¡@¥Y¥Y¥Y¥Y¥Y¥Y                  ");
+		System.out.println("¡@¡@¡@¡@¥Y                  ¥Y                  ");
+		System.out.println("¡@¡@¡@¡@¥Y                  ¥Y                  ");
+		System.out.println("¡@¡@¡@¡@¥Y                  ¥Y                  ");
+		System.out.println("¡@¡@¡@¡@¥Y                  ¥Y                  ");
+		System.out.println("¡@¡@¡@¡@¥Y                  ¥Y                  ");
+		System.out.println("¡@¡@¡@¡@¥Y                  ¥Y                  ");
+		System.out.println("¥Y¥Y¥Y¥Y¥Y                  ¥Y¥Y¥Y¥Y¥Y");
+		System.out.println("¥Y                                                      ¥Y");
+		System.out.println("¥Y                                                      ¥Y");
+		System.out.println("¥Y                                                      ¥Y");
+		System.out.println("¥Y                                                      ¥Y");
+		System.out.println("¥Y                                                      ¥Y");
+		System.out.println("¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y¥Y");
+	}
+
+}
