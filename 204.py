3c8d4c7fcd790ed5386099440cbad114ae9384c5
KuKuKai <KuKuKai@users.noreply.github.com>
2018-10-04 20:25:42 +0800

    Add files via upload
    python

---
+++ 204.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  4 20:15:08 2018
+
+@author: USER
+"""
+
+a=eval(input())
+b=eval(input())
+c=input()
+if(c=='*'):
+    print(a*b)
+elif(c=='/'):
+    print(a/b)
+elif(c=='+'):
+    print(a/b)
+elif(c=='-'):
+    print(a/b)
+elif(c=='//'):
+    print(a//b)
+elif(c=='%'):
+    print(a%b)
