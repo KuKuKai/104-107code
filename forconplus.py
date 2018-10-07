38693b9c7ca16177d9535607965863255f296b19
KuKuKai <KuKuKai@users.noreply.github.com>
2018-10-07 18:54:57 +0800

    TQC
    python

---
+++ forconplus.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  7 18:42:19 2018
+
+@author: JinJheng
+"""
+
+n=int(input())
+i=result=0
+while i<=n:
+    result=result+i
+    i=i+1
+print(result)
---
+++ whileconmult.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  7 18:47:37 2018
+
+@author: JinJheng
+"""
+
+n=int(input())
+result=1
+for i in range(1,n+1):
+    result=result*i
+    i=i+1
+print(result)
