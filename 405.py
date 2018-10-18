1bf9f5bfbd837d1ec9465e5af436c17759f6a8cc
KuKuKai <KuKuKai@users.noreply.github.com>
2018-10-19 00:00:59 +0800

    TQC
    python

---
+++ 405.py
@@ -0,0 +1,12 @@
+while True:
+  num=eval(input())
+  if num==-9999:
+    break
+  elif 90<=num<=100:
+    print("A")
+  elif 80<=num<90:
+    print("B")
+  elif 70<=num<80:
+    print("C")
+  elif 60<=num<70:
+    print("D")
