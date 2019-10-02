61a1df71362e09d9b42677632012d3966f1e322c
KuKuKai <KuKuKai@users.noreply.github.com>
2019-10-02 10:57:52 +0800

    speech_recognizer
    speech_recognizer GOOGLE API

---
+++ speechrecognizer.py
@@ -0,0 +1,18 @@
+import speech_recognition
+r = speech_recognition.Recognizer()
+
+with speech_recognition.Microphone() as source:
+    print("Please wait. Calibrating microphone...")
+    #listen for 5 seconds and create the ambient noise energy level
+    r.adjust_for_ambient_noise(source, duration=5)#調整麥克風的噪音
+    print('Say something!')
+    audio = r.listen(source)
+
+# recognize speech using Google Speech Recognition
+try:
+    print("Google Speech Recognition thinks you said:")
+    print(r.recognize_google(audio, language="zh-TW"))
+except speech_recognition.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except speech_recognition.RequestError as e:
+    print("No response from Google Speech Recognition service: {0}".format(e))
