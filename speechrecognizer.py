90d7615c4691ed00bba1517c7961baf7ca18d4da
KuKuKai <KuKuKai@users.noreply.github.com>
2019-10-07 15:38:35 +0800

    speech_recognizer
    speech_recognizer

--- speechrecognizer.py
+++ speechrecognizer.py
@@ -7,12 +7,19 @@ with speech_recognition.Microphone() as source:
     r.adjust_for_ambient_noise(source, duration=5)#調整麥克風的噪音
     print('Say something!')
     audio = r.listen(source)
+    
+    f = open('A.txt', 'w', encoding = 'UTF-8')    # 也可使用指定路徑等方式，如： C:\A.txt
 
 # recognize speech using Google Speech Recognition
 try:
     print("Google Speech Recognition thinks you said:")
     print(r.recognize_google(audio, language="zh-TW"))
+    f.write(r.recognize_google(audio, language="zh-TW"))
 except speech_recognition.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except speech_recognition.RequestError as e:
     print("No response from Google Speech Recognition service: {0}".format(e))
+
+#input()
+
+f.close()
